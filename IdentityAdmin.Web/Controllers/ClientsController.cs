16c1474ab31de06a6029a022e68c36a68d1114d5
Logan Anderson <logan.anderson@hcss.com>
2023-02-04 16:20:55 -0600

    Added post. Testing github push.

--- IdentityAdmin.Web/Controllers/ClientsController.cs
+++ IdentityAdmin.Web/Controllers/ClientsController.cs
@@ -4,8 +4,14 @@ namespace IdentityAdmin.Web.Controllers;
 
 public class ClientsController : ControllerBase
 {
-    // GET
+    [HttpGet]
     public async Task<IActionResult> Get()
+    {
+        return Ok("This is a test");
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateClient()
     {
         return Ok("This is a test");
     }
